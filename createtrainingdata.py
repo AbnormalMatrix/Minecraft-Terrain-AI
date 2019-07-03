dafac58429397fb4614eafd0e8d479e902a0e6e0
Skierman <babin.carter@gmail.com>
2019-07-02 19:47:09 -0600

    Add files via upload

--- createtrainingdata.py
+++ createtrainingdata.py
@@ -3,11 +3,13 @@ import numpy as np
 import cv2
 import pickle
 import time
+#from pynput import keyboard
+#from pynput.keyboard import Key, Listener
 from getkeys import key_check
-import pandas as pd
-from collections import Counter
-from random import shuffle
-seconds_to_run = int(input('How many seconds to run? ')) # run for a certain amount of seconds
+
+debug_frame = 0
+total_frame_session = 0
+
 # adw
 w = [1, 0, 0]
 a = [0, 1, 0]
@@ -31,7 +33,54 @@ def keys_to_output(keys):
         output = w
     return output
 
-training_data = []
+# current_label = None
+#
+#
+# def on_press(key):
+#     global current_label
+#
+#     print(key)
+#     if key == "a":
+#         current_label = 0
+#     elif key == "d":
+#         current_label = 1
+#     else:
+#         current_label = 2
+#
+#
+# def on_release(key):
+#     global current_label
+#
+#     current_label = 2
+#
+#
+# listener = Listener(on_press=on_press, on_release=on_release)
+# listener.start()
+
+'''
+def on_press(key):
+    print(key)
+    if key == "a":
+        training_data[1].append(possible_key_presses[0])
+    elif key == "d":
+        training_data[1].append(possible_key_presses[1])
+    else:
+        training_data[1].append(possible_key_presses[2])
+
+listener = keyboard.Listener(on_press=on_press)
+listener.start()
+'''
+
+#training_data = []
+try:
+    pickle_in = open("training_data.pickle", "rb")
+    training_data = pickle.load(pickle_in)
+    print("loading previous data")
+except:
+    print("starting fresh")
+    training_data = []
+
+pickle_out = open("training_data.pickle", "wb")
 
 print("3")
 time.sleep(1)
@@ -39,9 +88,25 @@ print("2")
 time.sleep(1)
 print("1")
 time.sleep(1)
-print("Creating training data for {0} seconds".format(str(seconds_to_run)))
-start_time = time.time()
-while(int(time.time()-start_time)<seconds_to_run):
+print("starting creation of training data!")
+
+'''while True:
+    for i in range(100):
+        with mss() as sct:
+            screenshot = sct.shot()
+            keys = key_check()
+            output = keys_to_output(keys)
+            image = cv2.imread(screenshot)
+            image = cv2.resize(image, (70, 70))
+            training_data.append([image / 255., output])
+    pickle.dump(training_data, pickle_out)
+    pickle_out.close()
+    pickle_in = open("training_data.pickle", "rb")
+    training_data = pickle.load(pickle_in)
+    print("loading previous data")
+    print(len(training_data))'''
+
+for i in range(1000):
     with mss() as sct:
         screenshot = sct.shot()
         keys = key_check()
@@ -49,50 +114,11 @@ while(int(time.time()-start_time)<seconds_to_run):
         image = cv2.imread(screenshot)
         image = cv2.resize(image, (70, 70))
         training_data.append([image / 255., output])
-        
-df = pd.DataFrame(training_data)
-print(df.head())
-print(Counter(df[1].apply(str)))
+        debug_frame = debug_frame + 1
+        if debug_frame == 100:
+            debug_frame = 0
+            total_frame_session = total_frame_session + 100
+            print(f"we are at {total_frame_session} samples")
 
-lefts = []
-rights = []
-forwards = []
-
-shuffle(training_data)
-
-for data in training_data:
-    img = data[0]
-    choice = data[1]
-    print(choice)
-
-    if choice == [0, 1, 0]:
-        lefts.append([img, choice])
-    elif choice == [0, 0, 1]:
-        rights.append([img, choice])
-    elif choice == [1, 0, 0]:
-        forwards.append([img, choice])
-    else:
-        print('no matches')
-
-min_samples = min(len(lefts), len(rights), len(forwards))
-forwards = forwards[:min_samples]
-lefts = lefts[:min_samples]
-rights = rights[:min_samples]
-
-print(len(forwards))
-print(len(lefts))
-print(len(rights))
-
-final_data = forwards + lefts + rights
-shuffle(final_data)
-
-balanced_training_data = [[], []]
-for data in final_data:
-    image = data[0]
-    choice = data[1]
-    balanced_training_data[0].append(image)
-    balanced_training_data[1].append(choice)
-
-pickle_out = open("training_data.pickle", "wb")
-pickle.dump(balanced_training_data, pickle_out)
+pickle.dump(training_data, pickle_out)
 pickle_out.close()
